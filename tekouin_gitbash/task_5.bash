8ee06026346302c7a26172c989f559cee3598f90
SelimaRobbana <robbana.selima@gmail.com>
2023-05-11 17:57:12 +0100

    Task 5 done

---
+++ tekouin_gitbash/task_5.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+count=0
+for dir in $(find . -type d -not -path '/\.' -not -path '.' -not -path '..'); do
+    count=$((count + 1))
+done
+echo "The number of directories and sub-directories in the current directory is: $count"
