305644c02545d141aa1f443c7ec3cb8103f3db01
SelimaRobbana <robbana.selima@gmail.com>
2023-05-12 02:33:25 +0100

    Task 9 done

---
+++ tekouin_gitbash/task_9.bash
@@ -0,0 +1,5 @@
+IFS= ":"
+IFS=:
+while read -r username password uid gid user_info home_dir shell; do
+  echo "The user $username is part of the $gid gang, lives in $home_dir and rides $shell. User ID's place is protected by the passcode $password, more info about the user here: $user_info"
+done < /etc/passwd
